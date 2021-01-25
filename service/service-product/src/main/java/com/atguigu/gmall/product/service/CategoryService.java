89219faf893941611375e6ca4484ac4c83640f16
cxy <zhangsan@163.com>
2021-01-25 21:47:51 +0800

    这是一个电商项目

--- service/service-product/src/main/java/com/atguigu/gmall/product/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.atguigu.gmall.product.service;
-
-import com.atguigu.gmall.product.bean.BaseCategory1;
-
-import java.util.List;
-
-public interface CategoryService {
-    List<BaseCategory1> getCategory1();
-}
